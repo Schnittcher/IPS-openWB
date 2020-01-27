8f033eeb377b91f8bca7cb9105e4e2e9603b7e20
Kai <kai.schnittcher@gmail.com>
2020-01-27 14:20:56 +0100

    Fix UpdateState

--- openWB/module.php
+++ openWB/module.php
@@ -144,7 +144,7 @@ require_once __DIR__ . '/../libs/.helper/VariableProfileHelper.php';
             foreach ($result as $key => $value) {
                 if (@$this->GetIDForIdent($key) != false) {
 					$this->SendDebug($this->GetIDForIdent($key), 'Key: ' . $key . ' - Value: ' . $value, 0);
-                    $this->SetValue($this->GetIDForIdent($key), $value);
+                    $this->SetValue($key, $value);
                 } else {
                     $this->SendDebug('Variable not exist', 'Key: ' . $key . ' - Value: ' . $value, 0);
                 }
