b5dcaa319a1f354cf394770003b1d272f8f22e4a
Kai <kai.schnittcher@gmail.com>
2020-01-28 08:58:28 +0100

    Fix Profile

--- openWB/module.php
+++ openWB/module.php
@@ -26,7 +26,7 @@ require_once __DIR__ . '/../libs/.helper/VariableProfileHelper.php';
 
             $this->RegisterProfileInteger('OWB.Ladeleistung', 'Electricity', '', 'A', 10, 32, 1);
             $this->RegisterProfileInteger('OWB.Minuten', '', 'Minuten', '', 0, 0, 1);
-            $this->RegisterProfileFloat('OWB.KM', '', '', 'km', 0, 0, 1);
+            $this->RegisterProfileFloat('OWB.KM', '', '', 'km', 0, 0, 1,1);
             $this->RegisterProfileFloat('OWB.Wh', '', '', 'Wh', 0, 0, 0.1, 1);
 
             $this->RegisterVariableInteger('lademodus', $this->Translate('Loading Mode'), 'OWB.Lademodus', 0);
