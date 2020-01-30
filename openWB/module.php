816799167f0cbfafa09dfe2a6f65dc9c69109e0b
Kai <kai.schnittcher@gmail.com>
2020-01-30 14:01:09 +0100

    Fix Helper

--- openWB/module.php
+++ openWB/module.php
@@ -1,7 +1,7 @@
 <?php
 
 declare(strict_types=1);
-require_once __DIR__ . '/../libs/.helper/VariableProfileHelper.php';
+require_once __DIR__ . '/../libs/helper/VariableProfileHelper.php';
 
     class openWB extends IPSModule
     {
